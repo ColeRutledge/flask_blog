36fc4634563430e4cd4eff0678639aa8e83035c4
Cole Rutledge <ColeRutledge@gmail.com>
2020-10-06 16:00:56 -0400

    add launch.json, tasks.json, and debugging config

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,18 @@
 ##### Pip Docker-Compose #####
 
-FROM python:3.8
+# FROM python:3.8
 
-ENV PIP_DISABLE_PIP_VERSION_CHECK=on
+# ENV PIP_DISABLE_PIP_VERSION_CHECK=on
 
-EXPOSE 5000
+# EXPOSE 5000
 
-WORKDIR /app
+# WORKDIR /app
 
-COPY requirements.txt .
+# COPY requirements.txt .
 
-RUN pip install --no-cache-dir -r requirements.txt
+# RUN pip install --no-cache-dir -r requirements.txt
 
-COPY . .
-
+# COPY . .
 
 
 ##### Pip No Docker-Compose #####
@@ -37,6 +36,32 @@ COPY . .
 
 # CMD [ "python", "-m", "flask", "run" ]
 
+
+##### Pip Docker-Compose w/ VSCode Debugger #####
+
+FROM python:3.8
+
+ENV PIP_DISABLE_PIP_VERSION_CHECK=on
+
+# Keeps Python from generating .pyc files in the container
+ENV PYTHONDONTWRITEBYTECODE 1
+# Turns off buffering for easier container logging
+ENV PYTHONUNBUFFERED 1
+
+EXPOSE 5000
+
+WORKDIR /app
+# ADD . /app
+
+COPY requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Switches to a non-root user and changes the ownership of the /app folder"
+# RUN useradd appuser && chown -R appuser /app
+# USER appuser
+
+# During debugging, this entry point will be overridden. For more information, please refer to https://aka.ms/vscode-docker-python-debug
+CMD ["gunicorn", "--bind", "0.0.0.0:5000", "app:app"]
 
 
 ##### Poetry #####
--- app.py
+++ app.py
@@ -1,26 +1,61 @@
 from flask import Flask, render_template
-import redis
-import time
+from datetime import datetime
+import re
+# import redis
+# import time
 
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis_cache', port=6379, decode_responses=True)
 app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
+# cache = redis.Redis(host='redis_cache', port=6379, decode_responses=True)
 
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
+# def get_hit_count():
+#     retries = 5
+#     while True:
+#         try:
+#             return cache.incr('hits')
+#         except redis.exceptions.ConnectionError as exc:
+#             if retries == 0:
+#                 raise exc
+#             retries -= 1
+#             time.sleep(0.5)
+
+
+# @app.route('/')
+# def hello():
+#     count = get_hit_count()
+#     return render_template('index.pug')
 
 
 @app.route('/')
-def hello():
-    count = get_hit_count()
-    return render_template('index.pug', count=count)
+def index():
+    user = {'username': 'Cole'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+    ]
+    return render_template('index.pug', user=user, title='Home', posts=posts)
+
+
+@app.route("/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+    # Filter the name argument to letters only using regular expressions. URL
+    # arguments can contain arbitrary text, so we restrict to safe characters.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return content
