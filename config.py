95b174e633d266425debd6d27d5cee100bc61835
Cole Rutledge <ColeRutledge@gmail.com>
2020-10-10 00:45:58 -0400

    format config

--- config.py
+++ config.py
@@ -18,10 +18,3 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + str(basedir.joinpath('app.db'))
         # 'sqllite:///' + basedir.joinpath('app.db').as_posix() # noqa
-
-
-# MAIL_SERVER = 'smtp.gmail.com'
-# MAIL_PORT = 587
-# MAIL_USE_TLS = 1
-# MAIL_USERNAME = 'ColeRutledge@gmail.com'
-# MAIL_PASSWORD = ":34DF9w_KG5^[y;="
