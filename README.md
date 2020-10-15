ede9b497573f377f79502007d410fd98ff88e8c7
Cole Rutledge <ColeRutledge@gmail.com>
2020-10-15 11:26:51 -0400

    add test config class and ipykernel to dev dep

--- README.md
+++ README.md
@@ -4,12 +4,14 @@
 
 - this project started as an attempt to experiment with redis and evolved into me working my way through the entire flask mega tutorial by Miguel Grinberg, a well-respected authority in the flask community. If you've any interest in learning some flask best practices by someone who wrote the o'reilly flask book and a couple of the popular packages in the flask ecosystem (flask-migrate, flask-moment, etc), I would give this a HUGE recommendation! amazing quality. <a href='https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world'>Flask Mega Tutorial by Miguel Grinberg</a>
 
-    - new things i've picked up from this project:
-        - project config using docker-compose and integration with vscode's debugger has been life changing! i'm beginning to understand the popularity of vscode.
-        - built bilingual language support (en, es) for entire site using flask-babel
+- this is a full-stack flask application that essentially mimics twitter. users can follow users. users can make posts of 140 characters or less and also have a profile page.
+
+    - few highlights that i've picked up from this project:
+        - built bilingual language support (en, es) for entire site using flask-babel. it has framework to build in many additional languages.
         - worked with azure's cognitive language translation api for identifying language dialects and follow-up conversion to the native browser language of the user. it allows for comments in foreign languages of other users to be translated in real-time with ajax
-        - mimic the asynchronous nature of javascript by using python's threading. In the project, this enabled background proccesses to handle sending emails for password recovery (computationally expensive) without blocking the main execution of the application.
+        - mimic the asynchronous nature of javascript by using python's threading. in the project, this enabled background proccesses to handle sending emails for password recovery (computationally expensive) without blocking the main execution of the application.
         - more robust error handling (logging and custom error handling) plus automated email notifications on ERROR level logging events
+        - project config using docker-compose and integration with vscode's debugger has been life changing! i'm beginning to understand the popularity of vscode. this has not yet been covered in the tutorial, but i used this project to sort it out, and it was WELL worth it.
 
     - other topics we've covered/built thus far:
         - both basic config & more advanced application factory pattern of configuring a flask app
@@ -19,10 +21,7 @@
         - session management with flask-login
         - real-time time deltas on user posts with flask-moment (port of moment.js)
         - sqlalchemy orm, models, migrations, and sqlite
-        - password management and email recovery via flask-mail
         - styles provided by flask-bootstrap's integration with wtforms
         - more to come!
-
-
 
 - also took some time to go through this: https://www.sqlitetutorial.net/sqlite-python -- sqlite is awesome!
--- test_user_model.py
+++ test_user_model.py
@@ -1,19 +1,31 @@
-from datetime import datetime, timedelta
-from app import app, db
+from app import create_app, db
 from app.models import User, Post
+from config import Config
+from datetime import datetime, timedelta
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+    # will have sqlite use an in-memory db for testing
+
+
+app = create_app(TestConfig)
+# app.app_context().push()
+# with app.app_context():
 
 
 # setup function that will run prior to each function test
 def setup_function():
-    # will have sqlite use an in-memory db for testing
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
-    db.create_all()
+    with app.app_context():
+        db.create_all()
 
 
 # teardown function that will run prior to each function test
 def teardown_function():
-    db.session.remove()
-    db.drop_all()
+    with app.app_context():
+        db.session.remove()
+        db.drop_all()
 
 
 def test_password_hashing():
@@ -32,59 +44,61 @@ def test_avatar():
 
 
 def test_follow():
-    u1 = User(username='john', email='john@example.com')
-    u2 = User(username='susan', email='susan@example.com')
-    db.session.add(u1)
-    db.session.add(u2)
-    db.session.commit()
-    assert u1.followed.all() == []
-    assert u1.followers.all() == []
+    with app.app_context():
+        u1 = User(username='john', email='john@example.com')
+        u2 = User(username='susan', email='susan@example.com')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        assert u1.followed.all() == []
+        assert u1.followers.all() == []
 
-    u1.follow(u2)
-    db.session.commit()
-    assert u1.is_following(u2) is True
-    assert u1.followed.count() == 1
-    assert u1.followed.first().username == 'susan'
-    assert u2.followers.count() == 1
-    assert u2.followers.first().username == 'john'
+        u1.follow(u2)
+        db.session.commit()
+        assert u1.is_following(u2) is True
+        assert u1.followed.count() == 1
+        assert u1.followed.first().username == 'susan'
+        assert u2.followers.count() == 1
+        assert u2.followers.first().username == 'john'
 
-    u1.unfollow(u2)
-    db.session.commit()
-    assert u1.is_following(u2) is False
-    assert u1.followed.count() == 0
-    assert u2.followers.count() == 0
+        u1.unfollow(u2)
+        db.session.commit()
+        assert u1.is_following(u2) is False
+        assert u1.followed.count() == 0
+        assert u2.followers.count() == 0
 
 
 def test_follow_posts():
-    # create four users
-    u1 = User(username='john', email='john@example.com')
-    u2 = User(username='susan', email='susan@example.com')
-    u3 = User(username='mary', email='mary@example.com')
-    u4 = User(username='david', email='david@example.com')
-    db.session.add_all([u1, u2, u3, u4])
+    with app.app_context():
+        # create four users
+        u1 = User(username='john', email='john@example.com')
+        u2 = User(username='susan', email='susan@example.com')
+        u3 = User(username='mary', email='mary@example.com')
+        u4 = User(username='david', email='david@example.com')
+        db.session.add_all([u1, u2, u3, u4])
 
-    # create four posts
-    now = datetime.utcnow()
-    p1 = Post(body='post from john', author=u1, timestamp=now + timedelta(seconds=1))
-    p2 = Post(body='post from susan', author=u2, timestamp=now + timedelta(seconds=4))
-    p3 = Post(body='post from mary', author=u3, timestamp=now + timedelta(seconds=3))
-    p4 = Post(body='post from david', author=u4, timestamp=now + timedelta(seconds=2))
-    db.session.add_all([p1, p2, p3, p4])
-    db.session.commit()
+        # create four posts
+        now = datetime.utcnow()
+        p1 = Post(body='post from john', author=u1, timestamp=now + timedelta(seconds=1))
+        p2 = Post(body='post from susan', author=u2, timestamp=now + timedelta(seconds=4))
+        p3 = Post(body='post from mary', author=u3, timestamp=now + timedelta(seconds=3))
+        p4 = Post(body='post from david', author=u4, timestamp=now + timedelta(seconds=2))
+        db.session.add_all([p1, p2, p3, p4])
+        db.session.commit()
 
-    # setup the followers
-    u1.follow(u2)           # john follows susan
-    u1.follow(u4)           # john follows david
-    u2.follow(u3)           # susan follows mary
-    u3.follow(u4)           # mary follows david
-    db.session.commit()
+        # setup the followers
+        u1.follow(u2)           # john follows susan
+        u1.follow(u4)           # john follows david
+        u2.follow(u3)           # susan follows mary
+        u3.follow(u4)           # mary follows david
+        db.session.commit()
 
-    # check the followed posts of each user
-    f1 = u1.followed_posts().all()
-    f2 = u2.followed_posts().all()
-    f3 = u3.followed_posts().all()
-    f4 = u4.followed_posts().all()
-    assert f1 == [p2, p4, p1]
-    assert f2 == [p2, p3]
-    assert f3 == [p3, p4]
-    assert f4 == [p4]
+        # check the followed posts of each user
+        f1 = u1.followed_posts().all()
+        f2 = u2.followed_posts().all()
+        f3 = u3.followed_posts().all()
+        f4 = u4.followed_posts().all()
+        assert f1 == [p2, p4, p1]
+        assert f2 == [p2, p3]
+        assert f3 == [p3, p4]
+        assert f4 == [p4]
