f197c270b51207b3b29276518317fbea99f4bd26
Cole Rutledge <ColeRutledge@gmail.com>
2020-10-26 13:47:32 -0400

    adds tokens to User model

--- app/models.py
+++ app/models.py
@@ -1,11 +1,13 @@
 from app import db, login
 from app.search import add_to_index, remove_from_index, query_index
-from datetime import datetime
+import base64
+from datetime import datetime, timedelta
 from flask import current_app, url_for
 from flask_login import UserMixin
 from hashlib import md5
 import json
 import jwt
+import os
 import redis
 import rq
 from time import time
@@ -60,6 +62,8 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     about_me = db.Column(db.String(140))
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     last_message_read_time = db.Column(db.DateTime)
     created_on = db.Column(db.DateTime, server_default=db.func.now())
@@ -97,6 +101,25 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return User
 
     def to_dict(self, include_email=False):
         data = {
