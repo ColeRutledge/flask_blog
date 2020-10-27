67e68a115bcac756fdd5963c6a98e581077e1373
Cole Rutledge <ColeRutledge@gmail.com>
2020-10-27 13:51:15 -0400

    deploy config

--- worker.py
+++ worker.py
@@ -1,22 +1,23 @@
 import os
-from redis import Redis
-from rq import Queue, Connection
-from rq.worker import HerokuWorker as Worker
-from urllib.parse import urlparse
+# from redis import Redis
+import redis
+from rq import Queue, Connection, Worker
+# from rq.worker import HerokuWorker as Worker
+# from urllib.parse import urlparse
 
 
 # listen = ['high', 'default', 'low']
 listen = ['flask_blog-tasks']
 
-redis_url = os.getenv('REDISTOGO_URL')
-if not redis_url:
-    raise RuntimeError('Set up Redis To Go first.')
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+# if not redis_url:
+#     raise RuntimeError('Set up Redis To Go first.')
 
 
-url = urlparse(redis_url)
-conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
+# url = urlparse(redis_url)
+# conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
 
-# conn = redis.from_url(redis_url)
+conn = redis.from_url(redis_url)
 
 
 if __name__ == '__main__':
